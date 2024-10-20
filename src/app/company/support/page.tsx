08dd8ff34e5b4a5e40056fe72f7d4df988ece237
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2024-10-20 14:31:58 -0400

    diverse-temp firs prototype commit

---
+++ src/app/company/support/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const CustomerSupportPage = (props: Props) => {
+  return <div>CustomerSupportPage</div>;
+};
+
+export default CustomerSupportPage;
